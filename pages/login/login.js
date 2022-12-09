0e1cc058d1025961522f6ba6c72e0d4776cea163
cxb <cxb@gmail.com>
2022-12-10 00:47:00 +0800

    添加weui库

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,4 @@
-// 首页 - 公告板
+// 首页 - 登陆
 const app = getApp()
 
 Page({
@@ -9,14 +9,9 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     // 如需尝试获取用户信息可改为false
-    canIUseOpenData: false //wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
   },
   // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad() {
     if (wx.getUserProfile) {
       this.setData({
@@ -27,16 +22,18 @@ Page({
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      desc: '集数助手获取微信信息仅用于关联集数账号',
       success: (res) => {
-        console.log('get user profile')
-        console.log(res)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
-        wx.redirectTo('logs')
       }
+    })
+  },
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
     })
   }
 })
