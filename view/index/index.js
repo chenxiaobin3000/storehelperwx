b3bbc3a07f05e7cadd8625750e2187ea3aa1f288
cxb <cxb@gmail.com>
2022-12-10 15:33:24 +0800

    修改目录

--- view/index/index.js
+++ view/index/index.js
@@ -1,18 +1,22 @@
 // logs.js
-const util = require('../../utils/util.js')
+const util = require('../../util/util.js')
 
 Page({
   data: {
     logs: []
   },
   onLoad() {
-    /*this.setData({
+    this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return {
           date: util.formatTime(new Date(log)),
           timeStamp: log
         }
       })
-    })*/
+    })
+  },
+  bindViewTap(tap) {
+    var id = tap.currentTarget.id
+    console.log(id)
   }
 })
